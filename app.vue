5cac98a44238681a0532ece1b5e0ca6ebdc9bc2b
Yves Thirion <thirion.yves@gmail.com>
2024-01-26 11:58:24 +0100

    feat: edtiable dropdown example

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+// https://primevue.org/dropdown/#editable
+const selectedCity = ref();
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
+</script>
 <template>
   <div>
-    <NuxtWelcome />
+    <Dropdown
+    v-model="selectedCity"
+    editable
+    :options="cities"
+    optionLabel="name"
+    placeholder="Select a City"
+    class="w-full md:w-14rem" />
   </div>
 </template>
